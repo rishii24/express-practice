7bb6aca5ac6fd9583d9c32fdea6692ebcded09a1
rishii24 <24rishi02@gmail.com>
2022-03-14 20:56:25 +0530

    error fixed

--- public/scripts/mapData.js
+++ public/scripts/mapData.js
@@ -4,12 +4,12 @@ async function getData() {
     const data = await response.json()
     console.log(data)
 
-    for (item of data) {
+    for (items of data) {
         const root = document.createElement('div')
         const geo = document.createElement('div')
-        geo.textContent = `you're here : ${item.lat, item.long}`
+        geo.textContent = `you're here : ${items.lat, items.long}`
         const time = document.createElement('div')
-        time.textContent = `at : ${item.date}`
+        time.textContent = `at : ${items.date}`
         root.append(geo, time)
         document.body.append(root)
     }
