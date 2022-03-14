ca6864e90cb8a45c6c0e0de147e01a8d094bdeae
rishii24 <24rishi02@gmail.com>
2022-03-14 20:55:17 +0530

    function call fixed

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -1,28 +1,29 @@
-const loca = () => {
-    if ('geolocation' in navigator) {
-        console.log("geolocation is available")
-        navigator.geolocation.getCurrentPosition(async position => {
-            const lat = position.coords.latitude
-            document.getElementById("lati").textContent = lat
-            const long = position.coords.longitude
-            document.getElementById("long").textContent = long
-            // console.log(position);
-            const date = Date()
-            // console.log(date)
-            const data = { lat, long, date }
-            const opt = {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }
-            const response = await fetch('/apicall', opt)
-            const adata = await response.json()
-            console.log(adata)
-        });
-    } else {
-        console.log("geolocation is not available")
+loca();
 
-    }
-}
+const loca = () => {
+  if ("geolocation" in navigator) {
+    console.log("geolocation is available");
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const lat = position.coords.latitude;
+      document.getElementById("lati").textContent = lat;
+      const long = position.coords.longitude;
+      document.getElementById("long").textContent = long;
+      // console.log(position);
+      const date = Date();
+      // console.log(date)
+      const data = { lat, long, date };
+      const opt = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      const response = await fetch("/apicall", opt);
+      const adata = await response.json();
+      console.log(adata);
+    });
+  } else {
+    console.log("geolocation is not available");
+  }
+};
